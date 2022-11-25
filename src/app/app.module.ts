6540459357285153b661918460a03b037158df40
mevshnu <vishnusivan1000@gmail.com>
2022-11-25 12:22:26 +0530

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewComponent } from './view/view.component';
+import { NavComponent } from './nav/nav.component';
+import { AddComponent } from './add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewComponent,
+    NavComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
